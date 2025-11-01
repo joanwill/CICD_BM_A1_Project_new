cde75ff0b812e960a34b1612d178efdbb6778108
brettwill <brettwill@datazug.ch>
2025-11-01 18:06:06 +0100

    import and cosmosdb

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -45,3 +45,36 @@ resource "azurerm_app_service" "prod-webapp" {
     DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_container_registry.prod-acr.admin_password
   }
 }
+
+
+resource "azurerm_cosmosdb_account" "prod-cosmos" {
+  name                = "bma1cosmos"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.prod-rg.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location          = var.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "prod-db" {
+  name                = "todo-db"
+  resource_group_name = azurerm_resource_group.prod-rg.name
+  account_name        = azurerm_cosmosdb_account.prod-cosmos.name
+}
+
+resource "azurerm_cosmosdb_sql_container" "prod-container" {
+  name                = "todo-items"
+  resource_group_name = azurerm_resource_group.prod-rg.name
+  account_name        = azurerm_cosmosdb_account.prod-cosmos.name
+  database_name       = azurerm_cosmosdb_sql_database.prod-db.name
+  partition_key_paths  = ["/pk"]
+  throughput          = 400
+}
