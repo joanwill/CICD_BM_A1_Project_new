0a8f8325af3e526692393371cec7921550fbe76f
brettwill <brettwill@datazug.ch>
2025-11-01 17:43:45 +0100

    added acr and webapp

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -19,3 +19,29 @@ resource "azurerm_app_service_plan" "prod-plan" {
     size = "B1"
   }
 }
+
+resource "azurerm_container_registry" "prod-acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.prod-rg.name
+  location            = var.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_app_service" "prod-webapp" {
+  name                = var.webapp_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.prod-rg.name
+  app_service_plan_id = azurerm_app_service_plan.prod-plan.id
+
+  site_config {
+    linux_fx_version = "DOCKER|${azurerm_container_registry.prod-acr.login_server}/todo-backend-api:latest"
+  }
+
+  app_settings = {
+    WEBSITES_PORT = "80"
+    DOCKER_REGISTRY_SERVER_URL      = "https://${azurerm_container_registry.prod-acr.login_server}"
+    DOCKER_REGISTRY_SERVER_USERNAME = azurerm_container_registry.acr.admin_username
+    DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_container_registry.acr.admin_password
+  }
+}
