1d9a0240b7f44991ea7925e2d45002538ca7e6d8
brettwill <brettwill@datazug.ch>
2025-11-01 22:57:56 +0100

    dev setup

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -22,6 +22,15 @@ resource "azurerm_app_service_plan" "plan" {
   }
 }
 
+resource "azurerm_container_registry" "acr" {
+  name                = "bma1pregproject"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+
 
 
 
