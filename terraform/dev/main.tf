7ecc94d538121bf878658071098baf362f658f08
joanwill <159011493+joanwill@users.noreply.github.com>
2025-11-02 10:37:36 +0100

    Update main.tf

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -30,6 +30,24 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 
+resource "azurerm_app_service" "webapp" {
+  name                = "todofrontendweb"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+
+  site_config {
+    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/fancy-todo-frontend:latest"
+  }
+
+  app_settings = {
+    WEBSITES_PORT = "80"
+    DOCKER_REGISTRY_SERVER_URL      = "https://${azurerm_container_registry.acr.login_server}"
+    DOCKER_REGISTRY_SERVER_USERNAME = azurerm_container_registry.acr.admin_username
+    DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_container_registry.acr.admin_password
+  }
+}
+
 
 
 
